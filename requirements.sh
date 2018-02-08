ecd76ad4bda373241ce8a8bae1cc6e2fdcbc8089
Richard Cotton <richierocks@gmail.com>
2018-02-08 10:07:03 -0500

    print isql version

--- requirements.sh
+++ requirements.sh
@@ -1,21 +1,7 @@
 # copied from https://github.com/datacamp/courses-msft-transact-sql
 pip3 install git+git://github.com/datacamp/sqlwhat-ext --no-deps
 
-# Follow sqlcmd instructions from
-# https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-setup-tools
+# Follow ideas from https://github.com/datacamp/docker-msft-sql-base/blob/master/requirements.sh
 
-# Add /opt/mssql-tools/bin/ to your PATH environment variable in a bash shell.
-export PATH="$PATH:/opt/mssql-tools/bin"
-export TERM=linux
-
-# Import the public repository GPG keys.
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-
-# Register the Microsoft Ubuntu repository.
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
-
-# Update the sources list and run the installation command with the unixODBC developer package.
-apt-get update && apt-get --yes install mssql-tools unixodbc-dev
-
-# Check it works
-sqlcmd -e -?
+# Simplest task: print isql version
+isql --version
