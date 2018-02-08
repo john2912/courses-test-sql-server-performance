f4b3ac9a9171149ec6e93d95ed0324b989d53521
Richard Cotton <richierocks@gmail.com>
2018-02-08 11:24:06 -0500

    run start_server.sh

--- requirements.sh
+++ requirements.sh
@@ -3,6 +3,9 @@ pip3 install git+git://github.com/datacamp/sqlwhat-ext --no-deps
 
 # Follow ideas from https://github.com/datacamp/docker-msft-sql-base/blob/master/requirements.sh
 
+# Start database server
+./start_server.sh
+
 # Simple task: return available tables
 isql Default sa $SA_PASSWORD -v 
 SELECT * FROM INFORMATION_SCHEMA.TABLES
