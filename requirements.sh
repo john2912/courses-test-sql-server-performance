e2e4d8ac2426e01db0295eea7ed087788e05259d
Richard Cotton <richierocks@gmail.com>
2018-02-12 13:41:45 -0500

    call start_server.sh

--- requirements.sh
+++ requirements.sh
@@ -5,5 +5,7 @@ pip3 install git+git://github.com/datacamp/sqlwhat-ext --no-deps
 
 wget https://s3.amazonaws.com/assets.datacamp.com/production/course_5315/datasets/create-co2-database.sql
 
+./start_server.sh
+
 # Simple task: return available tables
 isql Default sa $SA_PASSWORD -v < create-co2-database.sql
