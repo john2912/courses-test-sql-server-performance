26bc3b712cf5dbb7556d019dd0c008ec236206df
Richard Cotton <richierocks@gmail.com>
2018-02-12 13:15:21 -0500

    remove start_server.sh call

--- requirements.sh
+++ requirements.sh
@@ -3,8 +3,5 @@ pip3 install git+git://github.com/datacamp/sqlwhat-ext --no-deps
 
 # Follow ideas from https://github.com/datacamp/docker-msft-sql-base/blob/master/requirements.sh
 
-# Start database server
-./start_server.sh
-
 # Simple task: return available tables
 isql Default sa $SA_PASSWORD -v < test.sql
